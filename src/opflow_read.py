1a9b9db8021b6c6bffddf77cd81f4ce494ebdccd
Baguswijaya1 <bagusanw@gmail.com>
2025-09-02 19:27:10 +0700

    update opflow read

--- src/opflow_read.py
+++ src/opflow_read.py
@@ -2,6 +2,7 @@ from pymavlink import mavutil
 
 port = "/dev/ttyACM0"
 baudrate = 115200
+freq = 5
 
 fc = mavutil.mavlink_connection(device=port, baud=baudrate)
 fc.wait_heartbeat()
@@ -14,7 +15,7 @@ message = fc.mav.command_long_encode(
     mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL, # ID of command to send
     0, # confirmation
     mavutil.mavlink.MAVLINK_MSG_ID_OPTICAL_FLOW, # param 1 : message ID
-    100000, # param 2 : interval in usec
+    freq/1e6, # param 2 : interval in usec
     0, 0, 0, 0, 0 # param 3 - 7 (not used)
 )
 
@@ -23,10 +24,10 @@ fc.mav.send(message)
 while True:
     response = fc.recv_match(type="OPTICAL_FLOW", blocking=True, timeout=5.0)
     if response:
-        velx = response.flow_comp_m_x``
+        velx = response.flow_comp_m_x
         vely = response.flow_comp_m_y
         alt = response.ground_distance
 
-        print(f'velx = {velx}')
+        print(f'\nvelx = {velx}')
         print(f'vely = {vely}')
         print(f'alt = {alt}')
