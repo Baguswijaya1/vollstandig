1131fe2f574aa99758579dd30056a7682ddcff16
Baguswijaya1 <bagusanw@gmail.com>
2025-09-17 15:27:02 +0700

    update: add learn_mavros

--- build/low_level/build/lib/low_level/anti_collision.py
+++ build/low_level/build/lib/low_level/anti_collision.py
@@ -1,33 +1,33 @@
 import rclpy
 from rclpy.node import Node
-# from mavros_msgs.msg import DistanceSensor
+from sensor_msgs.msg import Range
 from mavros_msgs.msg import Altitude
 
 
 class Anti_collision(Node):
     def __init__(self):
         super().__init__('anti_collision')
-        # self.lidar_ = self.create_subscription(
-        #     msg_type=DistanceSensor,
-        #     topic='altitude',
-        #     callback=self.get_range(),
-        #     qos_profile=10
-        # )
-        self.altitude_ = self.create_subscription(
-            msg_type=Altitude,
-            topic='/mavros/altitude',
-            callback=self.get_alt,
+        self.lidar_ = self.create_subscription(
+            msg_type=Range,
+            topic='altitude',
+            callback=self.get_range(),
             qos_profile=10
         )
+        # self.altitude_ = self.create_subscription(
+        #     msg_type=Altitude,
+        #     topic='/mavros/altitude',
+        #     callback=self.get_alt,
+        #     qos_profile=10
+        # )
         self.get_logger().info('node started')
     
-    # def get_range(self, msg):
-    #     range = msg.current_distance
-    #     self.get_logger().info(f'range : {range}')
+    def get_range(self, msg):
+        range = msg.current_distance
+        self.get_logger().info(f'range : {range}')
 
-    def get_alt(self,msg):
-        alt = msg.terrain
-        self.get_logger().info(f'altitude : {alt}')
+    # def get_alt(self,msg):
+    #     alt = msg.terrain
+    #     self.get_logger().info(f'altitude : {alt}')
         
 
 def main(args=None):
---
+++ install/low_level/lib/python3.10/site-packages/low_level/learn_mavros.py
@@ -0,0 +1,50 @@
+import rclpy
+from rclpy.node import Node
+from mavros_msgs.msg import *
+from std_msgs.msg import Float64
+
+class Data_retrieve(Node):
+    def __init__(self):
+        super().__init__('data_retrieve')
+
+        self.attittude_subscription_ = self.create_subscription(
+            msg_type=Altitude,
+            topic='/mavros/altitude',
+            callback=self.print_alt,
+            qos_profile=10
+        )
+
+    def print_alt(self, msg):
+        self.get_logger().info(f'atlitude : {msg.monotonic}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Data_retrieve()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+# # ------------------------------------
+# class Data_retrieve(Node):
+#     def __init__(self):
+#         super().__init__('data_retrieve')
+#         self.altitude_subs = self.create_publisher(
+#             msg_type=Float64,
+#             topic = '/mavros/altitude',\
+#             callback = self.print_alt,
+#             qos_profile=10
+#         )
+
+#     def print_alt(self, msg):
+#         self.get_logger().info(f'altitude : {msg.data}')
+
+# def main(args=None):
+#     rclpy.init(args=args)
+#     node = Data_retrieve()
+#     rclpy.spin(node)
+#     node.destroy_node()
+#     rclpy.shutdown()
+
+# if __name__ == '__main__':
+#     main()
